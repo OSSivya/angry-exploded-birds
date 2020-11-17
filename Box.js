9f9f5d82fa5dcabdc3f5631ff7a0e91e5bc7dc63
OSSivya <73486402+OSSivya@users.noreply.github.com>
2020-11-17 15:31:39 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,41 @@
+class Box extends baseClass{
+  constructor(x, y, width, height) {
+  super(x,y,width,height);
+  this.image=loadImage("sprites/wood1.png");
+  
+  
+  
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
